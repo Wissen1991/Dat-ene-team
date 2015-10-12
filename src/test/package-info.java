99bb4629710ec73a649c4c3f0d42b888928db573
Sander van Wissen <Sander van Wissen@192.168.52.99>
2015-10-12 20:29:09 +0200

    test

---
+++ src/test/package-info.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class test
+{
+    public static void main(String[] args)
+    {
+
+        Scanner input = new Scanner(System.in);
+
+        double gewicht = 0.0;
+        double lengte = 0.0;
+        double bmi = 0.0;
+
+        System.out.print("Vul je gewicht in Kg: ");
+        gewicht = input.nextInt();
+
+        System.out.print("Vul je lengte in cm: ");
+        lengte = input.nextInt();
+
+        bmi = (gewicht/((lengte/100)*(lengte/100)));
+
+        System.out.printf("Je BMI is %f\n", bmi);
+
+        System.out.println("BMI Waardes");
+        System.out.println("Ondergewicht: Onder 18.5");
+        System.out.println("Normaal: 18.5-24.9 ");
+        System.out.println("Overgewicht: 25-29.9");
+        System.out.println("Obesitas: 30 of hoger");
+    }
+}
+
+
