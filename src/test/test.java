7a46328ed93003b6bd3ee572cc5a50111d6024c3
Joost Janssen <jjanssen@kza.nl>
2015-11-02 11:08:42 +0100

    Advies aangepast zodat alleen de van toepassing zijnde waardes worden teruggekoppeld.

--- src/test/test.java
+++ src/test/test.java
@@ -22,12 +22,16 @@ public class test
 
         System.out.printf("Je BMI is %f\n", bmi);
 
-        System.out.println("BMI Waardes");
-        System.out.println("Ondergewicht: Onder 18.5");
-        System.out.println("Normaal: 18.5-24.9 ");
-        System.out.println("Overgewicht: 25-29.9");
-        System.out.println("Obesitas: 30 of hoger");
+        System.out.println("BMI Waarde");
+        
+        if (bmi < 18.5) {
+        	System.out.println("Ondergewicht: Onder 18,5");
+        } else if (18.4<bmi && bmi< 25.0) {
+        	System.out.println("Normaal: 18,5-24,9");
+        } else if (24.9<bmi && bmi<30.0) {
+        	System.out.println("Overgewicht: 25-29,9");
+        } else if (bmi > 29.9){	
+        	System.out.println("Obesitas: 30 of hoger");
+        }
     }
 }
-
-
