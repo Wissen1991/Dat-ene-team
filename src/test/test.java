2f0f204f5ba4acc4c00290315ad9f6d658457a13
Joost Janssen <jjanssen@kza.nl>
2015-11-02 18:24:01 +0100

    Advies verder uitgebreid

--- src/test/test.java
+++ src/test/test.java
@@ -26,12 +26,16 @@ public class test
         
         if (bmi < 18.5) {
         	System.out.println("Ondergewicht: Onder 18,5");
+        	System.out.println("Doe eens effe snel naar McDonalds gaan!");
         } else if (18.4<bmi && bmi< 25.0) {
         	System.out.println("Normaal: 18,5-24,9");
+        	System.out.println("Goed bezig, volhouden!");
         } else if (24.9<bmi && bmi<30.0) {
         	System.out.println("Overgewicht: 25-29,9");
+        	System.out.println("Tijd voor de sportschool!");
         } else if (bmi > 29.9){	
         	System.out.println("Obesitas: 30 of hoger");
+        	System.out.println("Raadpleeg professionele hulp of ga op het strand liggen, dan word je vanzelf weer de zee ingetrokken!");
         }
     }
 }
