1c41f638eaebc5daa4ce4a8c079d6ef05b06cb2f
Joost Janssen <jjanssen@kza.nl>
2015-11-13 16:17:53 +0100

    Verder uitgebreid met geslacht en het advies gestructureerd en decimalen gefixed

--- src/test/test.java
+++ src/test/test.java
@@ -48,7 +48,11 @@ public class test
         double tailleBovenwaarde = 80.0;
 
         while (true) {
-        	
+
+	        System.out.print("Vul je geslacht in (m/v): ");
+	        Scanner scanner = new Scanner(System.in);
+	        String geslacht = scanner.nextLine().trim();
+	        	        
 	        System.out.print("Vul je gewicht in Kg: ");
 	        gewicht = input.nextInt();
 	
@@ -56,30 +60,27 @@ public class test
 	        lengte = input.nextInt();
 	     
 	        bmi = (gewicht/((lengte/100)*(lengte/100)));
-	
-	        System.out.printf("Je BMI is %f\n", bmi);
-	
+       
+            System.out.print("Vul de omvang van je taille in: ");
+            taille = input.nextInt();
+            
+            System.out.println("");
+            System.out.println("Je hebt opgegeven:");
+            System.out.printf("Geslacht: %s\n", geslacht);
+            System.out.printf("Lengte: %.0f cm\n", lengte);
+            System.out.printf("Gewicht: %.1f kg\n", gewicht);
+            System.out.printf("Taille: %.0f cm\n", taille);
+            System.out.println("");
+	        System.out.printf("Je BMI is %.2f\n", bmi);
+	        System.out.println("");
 	        System.out.println("BMI Waarde");
-	        
+	    
 	        if (bmi < 18.5) {
 	        	System.out.println("Ondergewicht: Onder 18,5");
 	        	System.out.println("Doe eens effe snel naar McDonalds gaan!");
 	        } else if (18.4<bmi && bmi< 25.0) {
 	        	System.out.println("Normaal: 18,5-24,9");
-	        	System.out.println("Goed bezig, volhouden!");
-	        
-		            System.out.print("Vul de omvang van je taille in: ");
-		            taille = input.nextInt();
-		            System.out.println("Taille:");
-		            if (taille < tailleOnderwaarde ) {
-		            	System.out.println("Je hebt een kleine taille"); 
-		            }
-		            else if (tailleOnderwaarde<=taille && taille< tailleBovenwaarde) {
-		            	System.out.println("Je taille is ook top!");
-		            }
-		            else if (taille>=tailleBovenwaarde) {
-		            	System.out.println("Denk om je buik!");
-		            }            
+	        	System.out.println("Goed bezig, volhouden!");        
 	        } 
 	        else if (24.9<bmi && bmi<30.0) {
 	        	System.out.println("Overgewicht: 25-29,9");
@@ -88,7 +89,23 @@ public class test
 	        	System.out.println("Obesitas: 30 of hoger");
 	        	System.out.println("Raadpleeg professionele hulp of ga op het strand liggen, dan word je vanzelf weer de zee ingetrokken!");
 	        }
-
+	        
+	        System.out.println("");
+	    	System.out.println("Je taille is:");
+	        if (taille < tailleOnderwaarde ) {
+            	System.out.println("Niet ok, je hebt een kleine taille"); 
+            }
+            else if (tailleOnderwaarde<=taille && taille< tailleBovenwaarde) {
+            	System.out.println("Ok, je taille is top!");
+            }
+            else if (taille>=tailleBovenwaarde) {
+            	System.out.println("Niet ok, denk om je buik!");
+            }   
+	        
+	        System.out.println("");
+	        System.out.println("Disclaimer: deze uitkomsten kunnen geen rechten aan ontleend worden.");
+	        System.out.println("");
+	        
 	        if (nogEenKeer()){
 	        	continue;
 	        }
